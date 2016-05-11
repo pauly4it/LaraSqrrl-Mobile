94d600d5ae9591fca0bf62973e13263b53094ad6
Paul Foryt <paulyf@gmail.com>
2016-05-11 11:53:24 -0700

    Build out app.js
    - add AppController
    - add states

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,24 +1,57 @@
-// Ionic Starter App
+// LaraSqrrl mobile app
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var APP = angular
+  .module('larasqrrl', ['ionic', 'ngResource'])
+  .run(function($ionicPlatform) {
+    console.log('loaded core');
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // hide accessory bar by default
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        // prevents the view from scrolling when input is in focus
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        // style the status bar
+        StatusBar.styleBlackTranslucent();
+      }
+    });
+  })
+  .controller('AppController', ["$scope", "$resource", '$state', '$ionicHistory', 'CurrentUser', function ($scope, $resource, $state, $ionicHistory, CurrentUser) {
+    $scope.logout = function() {
+      // don't allow back navigation on logout
+      $ionicHistory.nextViewOptions({
+        disableBack: true
+      });
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+      // destroy the current user service
+      CurrentUser.destroy();
+
+      // navigate to login screen
+      $state.go('app');
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+
+  }])
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/',
+        templateUrl: 'partials/login.html',
+        controller: 'LoginController'
+      })
+
+      .state('feed', {
+        url: '/feed',
+        templateUrl: 'partials/feed.html',
+        controller: 'FeedController'
+      });
+
+    $urlRouterProvider.otherwise('/');
+
+    $ionicConfigProvider.views.transition('android');
+    $ionicConfigProvider.views.swipeBackEnabled(false);
+
+    $httpProvider.defaults.useXDomain = true;
   });
-})
